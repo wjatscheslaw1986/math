634d400af9b43a073be3b3bb42917685e8735dde
Viacheslav Mikhailow <vm@MacBook-Pro-Viacheslav.local>
2022-11-17 13:08:07 -0800

    transponded, reversed, cofactors

--- src/app/app/App.java
+++ src/app/app/App.java
@@ -11,10 +11,12 @@ public class App {
         cap.payRespects();
         freak.freakOut();
         
-        //int[][] matrix = {{5, 1, 5},{2, 3, 4},{6, 4, 21}};
-        //int[][] result = IntMatrixCalc.multiplyOnInt(matrix, 2);
-        //System.out.print(IntMatrixCalc.print(matrix));
-    
+        int[][] matrix = {{5, 1, 5},{2, 3, 4},{6, 4, 21}};
+        int[][] result = IntMatrixCalc.multiplyMatrixOnInt(matrix, 2);
+        System.out.print(IntMatrixCalc.print(matrix));
+        System.out.println("");
+        System.out.print(IntMatrixCalc.print(result));
+        System.out.println("");
         int[][] matrix0 = {{ 1, 2, 3 },{ 4, -3, 1 }}, matrix1 = {{ 0, 5, 1 },{ -4, 7, 4 }};
         System.out.print(IntMatrixCalc.print(IntMatrixCalc.sumMatrices(matrix0, matrix1)));
         System.out.println("");
@@ -30,6 +32,70 @@ public class App {
         int[][] matrix8 = {{ 4, 5, 1 },{ 7, 1, 3 }}, matrix9 = {{ 2, 4  },{ 5, 1 }};
         System.out.print(IntMatrixCalc.print(IntMatrixCalc.multiplyMatrices(matrix9, matrix8)));
         System.out.println("");
+        
+        int[][] matrix10 = {{ 3, 2 },{ 1, 4 }};
+        System.out.print(IntMatrixCalc.print(IntMatrixCalc.transpondMatrix(matrix10)));
+        int[][] matrix11 = {{ 1, 3, -5 }};
+        System.out.print(IntMatrixCalc.print(IntMatrixCalc.transpondMatrix(matrix11)));
+        
+        int[][] matrix12 = {{ 7, 2 },{ 5, 3 }};
+        System.out.print(IntMatrixCalc.det(matrix12));
+        System.out.println("");
+        
+        int[][] matrix13 = {{ 1, 3, -2},{ 4, 2, 5 },{ -3, 1, 7}};
+        System.out.print(IntMatrixCalc.det(matrix13));
+        System.out.println("");
+        
+        int[][] matrix14 = {{ 2, -4, 3, 1 },{ 5, -1, 0, 4 },{ 2, 3, 0, 1 },{ -3, 4, 2, 0 }};
+        System.out.print(IntMatrixCalc.det(matrix14));
+        System.out.println("");
+        
+        int[][] matrix15 = {{ 8, 5, 2, -4 },{ 2, 7, 0, 1 },{ 4, 4, 1, -2 },{ 3, -2, 5, 1 }};
+        System.out.print(IntMatrixCalc.det(matrix15));
+        System.out.println("");
+        
+        int[][] matrix16 = {{ -9, -22 },{ 15, -25 }}, matrix17 = {{ 3, -4 },{ 5, 1 }};
+        System.out.print(IntMatrixCalc.print(
+            IntMatrixCalc.sumMatrices(
+                IntMatrixCalc.multiplyMatrixOnInt(IntMatrixCalc.multiplyMatrices(matrix17, matrix17), 3),
+                IntMatrixCalc.multiplyMatrixOnInt(matrix16, -2)
+        ))
+        );
+        System.out.print(
+            IntMatrixCalc.det(IntMatrixCalc.sumMatrices(
+                IntMatrixCalc.multiplyMatrixOnInt(IntMatrixCalc.multiplyMatrices(matrix17, matrix17), 3),
+                IntMatrixCalc.multiplyMatrixOnInt(matrix16, -2)
+        )));
+        System.out.println("");
+        
+        int[][] matrix18 = {{ 3, -4 },{ 2, 5 }}, matrix19 = {{ -1, 5 },{ -2, -3 }};
+        System.out.print(IntMatrixCalc.print(IntMatrixCalc.sumMatrices(
+            IntMatrixCalc.multiplyMatrices(matrix18, matrix19),
+            IntMatrixCalc.multiplyMatrixOnInt(IntMatrixCalc.multiplyMatrices(matrix19, matrix18), -1)
+        )));
+        System.out.println("");
+        
+        int[][] matrix20 = {{ 1, 3, -1 },{ 0, 5, 2 },{ 3, -2, 4 }}, matrix21 = {{ 1, 2, 0 },{ 3, 1, 2 },{ -4, 1, 5 }};
+        System.out.print(IntMatrixCalc.print(IntMatrixCalc.multiplyMatrices(matrix20, matrix21)));
+        System.out.println("");
+        
+        int[][] matrix22 = {{ 2, 0, 2 },{ -3, 2, 0 },{ 6, -2, 4 }};
+        System.out.println("`det:` " + IntMatrixCalc.det(matrix22));
+        System.out.println("");
+        System.out.println("cofactor A11 : " + IntMatrixCalc.cofactor(matrix22, 1, 1));
+        System.out.println("");
+        System.out.println("cofactors matrix : \n" + IntMatrixCalc.print(IntMatrixCalc.cofactorsMatrix(matrix22)));
+        System.out.println("");
+        System.out.println("" + IntMatrixCalc.print(IntMatrixCalc.transpondMatrix(IntMatrixCalc.cofactorsMatrix(matrix22))));
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println(IntMatrixCalc.print(IntMatrixCalc.reverse(matrix22)));
+        System.out.println("");
+        
+        
+        
+        
         // System.out.println(IntMatrixCalc.canMultiply(matrix7, matrix6));
 //         System.out.println(IntMatrixCalc.canMultiply(matrix6, matrix7));
 //         System.out.println(matrix6.length);
--- src/linear/matrix/IntMatrixCalc.java
+++ src/linear/matrix/IntMatrixCalc.java
@@ -1,16 +1,43 @@
 package matrix;
 
+import java.math.BigDecimal;
+
 public class IntMatrixCalc {
     
-    public static int[][] multiplyOnInt(int[][] matrix, int multiplier) {
+    public static int[][] multiplyMatrixOnInt(int[][] matrix, int multiplier) {
         validateMatrix(matrix);
-        for (int rowNum = 0 ; rowNum < matrix.length; rowNum++)
+        int[][] result = new int[matrix.length][matrix[0].length];
+        for (int rowNum = 0 ; rowNum < matrix.length; rowNum++) {
+            System.arraycopy(matrix[rowNum], 0, result[rowNum], 0, matrix[rowNum].length);
             for (int colNum  = 0; colNum < matrix[rowNum].length; colNum++) {
-                int val = matrix[rowNum][colNum];
-                val = val * multiplier;
-                matrix[rowNum][colNum] = val;
+                result[rowNum][colNum] = result[rowNum][colNum] * multiplier;
             }
-        return matrix;   
+        }
+        return result;   
+    }
+    
+    public static double[][] multiplyMatrixOnNumber(int[][] matrix, double multiplier) {
+        validateMatrix(matrix);
+        double[][] result = new double[matrix.length][matrix[0].length];
+        for (int rowNum = 0 ; rowNum < matrix.length; rowNum++) {
+            for (int c = 0; c < matrix[0].length; c++) result[rowNum][c] = (double) matrix[rowNum][c];
+            for (int colNum  = 0; colNum < matrix[rowNum].length; colNum++) {
+                result[rowNum][colNum] = result[rowNum][colNum] * multiplier;
+            }
+        }
+        return result;   
+    }
+    
+    public static double[][] multiplyMatrixOnNumber(double[][] matrix, double multiplier) {
+        //validateMatrix(matrix);
+        double[][] result = new double[matrix.length][matrix[0].length];
+        for (int rowNum = 0 ; rowNum < matrix.length; rowNum++) {
+            System.arraycopy(matrix[rowNum], 0, result[rowNum], 0, matrix[rowNum].length);
+            for (int colNum  = 0; colNum < matrix[rowNum].length; colNum++) {
+                result[rowNum][colNum] = result[rowNum][colNum] * multiplier;
+            }
+        }
+        return result;   
     }
     
     public static int[][] multiplyMatrices(int[][] matrixLeft, int[][] matrixRight) {
@@ -26,8 +53,79 @@ public class IntMatrixCalc {
         return result;
     }
     
+    public static int[][] transpondMatrix(int[][] matrix) {
+        validateMatrix(matrix);
+        int[][] result = new int[matrix[0].length][matrix.length];
+        for (int row = 0; row < matrix.length; row++) 
+            for (int col = 0; col < matrix[0].length; col++)
+                result[col][row] = matrix[row][col];
+        return result;
+    }
+    
+    public static int det(int[][] matrix) {
+        validateSquareMatrix(matrix);
+        int result = Integer.MAX_VALUE;
+        if (matrix.length == 2) return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0];
+        if (matrix.length == 3) return detSarrus(matrix);
+        if (matrix.length > 3) {
+            result = 0;
+            for(int col = 0; col < matrix[0].length; col++) {
+                if (matrix[0][col] != 0)
+                result = result + matrix[0][col] * cofactor(matrix, 1, col + 1);
+            }
+        }
+        return result;
+    }
+    
+    public static int detSarrus(int[][] matrix) {
+        validateSquareMatrix(matrix);
+        return matrix[0][0]*matrix[1][1]*matrix[2][2] + matrix[0][1]*matrix[1][2]*matrix[2][0] + matrix[1][0]*matrix[2][1]*matrix[0][2] - matrix[0][2]*matrix[1][1]*matrix[2][0] - matrix[1][0]*matrix[0][1]*matrix[2][2] - matrix[2][1]*matrix[1][2]*matrix[0][0];
+        
+    }
+    
+    public static double[][] reverse(int[][] matrix) {
+        validateSquareMatrix(matrix);
+        int det = det(matrix);
+        if (det == 0) throw new IllegalArgumentException("The matrix is degenerate, so cannot have an inverted one");
+        return multiplyMatrixOnNumber(transpondMatrix(cofactorsMatrix(matrix)), 1.0d/det);
+    }
+    
+    /**
+    BEWARE: row and col are not Java array's indices but math matrix' row and column numbers instead, from 1 to matrix.length
+    */
+    public static int cofactor(int[][] matrix, int row, int col) {
+        return (int) Math.round(Math.pow(-1, row + col)) * det(submatrix(matrix, row, col));
+    }
+    
+    public static int[][] cofactorsMatrix(int[][] matrix) {
+        int[][] result = new int[matrix.length][matrix.length];
+        for (int row = 1; row <= matrix.length; row++)
+            for (int col = 1; col <= matrix.length; col++)
+                result[row-1][col-1] = cofactor(matrix, row, col);
+        return result;
+    }
+    
+    /**
+    BEWARE: row and col are not Java array's indices but math matrix' row and column numbers instead, from 1 to matrix.length
+    */
+    public static int[][] submatrix(int[][] matrix, int row, int col) {
+        int[][] submatrix = new int[matrix.length-1][matrix.length-1];
+         int submatrixRow = 1, submatrixCol = 1;
+         for (int rowNum = 1; rowNum <= matrix.length; rowNum++) {
+            if (rowNum == row) continue;
+            for (int colNum = 1; colNum <= matrix.length; colNum++) {
+                if (colNum == col) continue;
+                submatrix[submatrixRow-1][submatrixCol-1] = matrix[rowNum-1][colNum-1];
+                submatrixCol++;
+            }
+            submatrixCol = 1;
+            submatrixRow++;
+        }
+        return submatrix;
+    }
+    
     public static int[][] sumMatrices(int[][] matrix1, int[][] matrix2) {
-        if (!canSum(matrix1, matrix2)) throw new IllegalArgumentException("Matrices need to be of equal size if you want to sum them");
+        if (!canSum(matrix1, matrix2)) throw new IllegalArgumentException("Matrices need to be of equal size if you want to sum them up");
         int[][] result = new int[matrix1.length][matrix1[0].length];
         for (int row = 0; row < matrix1.length; row++) 
             for (int col = 0; col < matrix1[0].length; col++)
@@ -52,16 +150,50 @@ public class IntMatrixCalc {
         return sb.toString();
     }
     
+    public static String print(double[][] matrix) {
+        int[] columnWiths = getPrintedColumnWiths(matrix);
+        StringBuilder sb = new StringBuilder();
+        for (int rowNum = 0; rowNum < matrix.length; rowNum++) {
+            for (int colNum = 0; colNum < matrix[rowNum].length; colNum++) {
+                if (colNum == 0) sb.append("| ");
+                sb.append(" ").append(String.valueOf(matrix[rowNum][colNum])).append(" ");
+                for (int j = 0; j <= columnWiths[colNum] - String.valueOf(matrix[rowNum][colNum]).length(); j++) {
+                    sb.append(" ");
+                }
+                if(colNum == matrix[rowNum].length - 1) sb.append(" |");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+    
+    public static void validateSquareMatrix(int[][] matrix) {
+        validateMatrix(matrix);
+        if (matrix.length < 2 || matrix.length != matrix[0].length) throw new IllegalArgumentException("Not a square matrix");
+    }
+    
     public static void validateMatrix(int[][] matrix) {
-        if (isEmpty(matrix)) throw new IllegalArgumentException("Matrix must have at least one element");
+        if (isEmpty(matrix)) throw new IllegalArgumentException("Not a matrix argument. A matrix must have at least one element");
         int colNum = matrix[0].length;
         for (int i = 1; i < matrix.length; i++) {
-            if (matrix[i].length != colNum) throw new IllegalArgumentException("All rows of the matrix must be of equal length");
+            if (matrix[i].length != colNum) throw new IllegalArgumentException("Malformed matrix argument. All rows of the matrix must be of equal length");
         }
     }
     
     private static int[] getPrintedColumnWiths(int[][] matrix) {
         validateMatrix(matrix); 
+        int[] result = new int[matrix[0].length];
+        for (int rowNum = 0; rowNum < matrix.length; rowNum++) {
+            for (int colNum = 0; colNum < matrix[rowNum].length; colNum++) {
+                int stringLength = String.valueOf(matrix[rowNum][colNum]).length();
+                if (stringLength > result[colNum]) result[colNum] = stringLength;
+            }
+        }
+        return result;
+    }
+    
+    private static int[] getPrintedColumnWiths(double[][] matrix) {
+        //validateMatrix(matrix); 
         int[] result = new int[matrix[0].length];
         for (int rowNum = 0; rowNum < matrix.length; rowNum++) {
             for (int colNum = 0; colNum < matrix[rowNum].length; colNum++) {
