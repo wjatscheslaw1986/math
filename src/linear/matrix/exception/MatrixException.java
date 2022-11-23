b2498f63113013630e0f8c6e94800fdabfcbaa04
Viacheslav Mikhailow <vm@MacBook-Pro-Viacheslav.local>
2022-11-23 12:13:15 -0800

    Alternative method to resolve Cramer Linear Equations. Comments. Copyright

--- src/linear/matrix/exception/MatrixException.java
+++ src/linear/matrix/exception/MatrixException.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2022. This code has an author and the author is Viacheslav Mikhailov, which is me.
+ * You may contact me via EMail taleskeeper@yandex.ru
+ */
+
 package matrix.exception;
 
 public class MatrixException extends Exception {
