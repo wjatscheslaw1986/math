2a05985420a48ec109a9084a8e8a2046470f6c05
Viacheslav Mikhailow <vm@MacBook-Pro-Viacheslav.local>
2022-11-24 10:21:03 -0800

    spatial package. Vector object

---
+++ src/linear/spatial/Vector.java
@@ -0,0 +1,19 @@
+/*≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+ ≈ Copyright © 2022. This code has an author and the author is me.
+ ≈ My name is Viacheslav Mikhailov.
+ ≈ You may contact me via EMail taleskeeper@yandex.ru
+ ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈*/
+
+package spatial;
+
+public class Vector {
+    private double[] coordinates;
+
+    public Vector(double[] coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public double[] getCoordinates() {
+        return coordinates;
+    }
+}
---
+++ src/linear/spatial/exception/VectorException.java
@@ -0,0 +1,13 @@
+/*≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+ ≈ Copyright © 2022. This code has an author and the author is me.
+ ≈ My name is Viacheslav Mikhailov.
+ ≈ You may contact me via EMail taleskeeper@yandex.ru
+ ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈*/
+
+package spatial.exception;
+
+public class VectorException extends Exception {
+    public VectorException(String message) {
+        super(message);
+    }
+}
