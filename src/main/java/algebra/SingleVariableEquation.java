ce648caac47ad7bb1e55446cb66f7044cdc3a3dc
Wjatscheslaw Michailov <mikhailowvw@gmail.com>
2025-03-08 11:21:42 +0300

    Single Variable Equations
    Sum, multiplication, distinction of members, tests.

---
+++ src/main/java/algebra/SingleVariableEquation.java
@@ -0,0 +1,14 @@
+/**
+ * Viacheslav Mikhailov (taleskeeper@yandex.com)  © 2025.
+ */
+package algebra;
+
+import java.util.List;
+
+/**
+ * An equation with single variable with 0 (zero) in its right part (i.e. after the '=').
+ *
+ * @author Viacheslav Mikhailov
+ */
+public record SingleVariableEquation(List<Member> members){
+}
